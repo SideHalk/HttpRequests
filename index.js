9fb7a209a0f889aece24ef715912739ea3e3669d
Side Halk <35532148+SideHalk@users.noreply.github.com>
2018-01-17 15:29:56 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+//GET Request
+
+function GETrequest(URL,callback) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+         //Action
+         callback(this.responseText);
+      }
+  };
+  
+  xhttp.open("GET", URL, true);
+  xhttp.send();
+  
+}
+
+//POST
+
+function GETrequest(URL,obj,callback) {
+  var objJSON = JSON.stringify(obj);
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+            callback(this.responseText);
+    }
+  };
+  
+  xhttp.open("POST", URL, true);
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  xhttp.send(objJSON);
+  
+}
